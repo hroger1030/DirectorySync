451d531c1e6a8485cf2c470061098ae4bad6fee7
roger.hill <hroger1030@gmail.com>
2017-04-09 01:53:47 -0700

    Initial Upload

---
+++ DirectorySync/Main.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace DirectoryCopy
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnSync = new System.Windows.Forms.Button();
+            this.txtSourceDir = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDestinationDir = new System.Windows.Forms.TextBox();
+            this.btnSelectSource = new System.Windows.Forms.Button();
+            this.btnDestination = new System.Windows.Forms.Button();
+            this.btnClearLog = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.RichTextBox();
+            this.lblAbout = new System.Windows.Forms.Label();
+            this.chkTestMode = new System.Windows.Forms.CheckBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.pbFiles = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnSync
+            // 
+            this.btnSync.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSync.Location = new System.Drawing.Point(287, 238);
+            this.btnSync.Name = "btnSync";
+            this.btnSync.Size = new System.Drawing.Size(131, 23);
+            this.btnSync.TabIndex = 0;
+            this.btnSync.Text = "Syncronize Directories";
+            this.btnSync.UseVisualStyleBackColor = true;
+            this.btnSync.Click += new System.EventHandler(this.btnSync_Click);
+            // 
+            // txtSourceDir
+            // 
+            this.txtSourceDir.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSourceDir.Location = new System.Drawing.Point(128, 12);
+            this.txtSourceDir.Name = "txtSourceDir";
+            this.txtSourceDir.Size = new System.Drawing.Size(335, 20);
+            this.txtSourceDir.TabIndex = 1;
+            this.txtSourceDir.TextChanged += new System.EventHandler(this.txtSourceDir_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Source Directory:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Destination Directory:";
+            // 
+            // txtDestinationDir
+            // 
+            this.txtDestinationDir.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDestinationDir.Location = new System.Drawing.Point(126, 46);
+            this.txtDestinationDir.Name = "txtDestinationDir";
+            this.txtDestinationDir.Size = new System.Drawing.Size(335, 20);
+            this.txtDestinationDir.TabIndex = 4;
+            this.txtDestinationDir.TextChanged += new System.EventHandler(this.txtDestinationDir_TextChanged);
+            // 
+            // btnSelectSource
+            // 
+            this.btnSelectSource.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelectSource.Location = new System.Drawing.Point(469, 10);
+            this.btnSelectSource.Name = "btnSelectSource";
+            this.btnSelectSource.Size = new System.Drawing.Size(111, 23);
+            this.btnSelectSource.TabIndex = 6;
+            this.btnSelectSource.Text = "Select Source";
+            this.btnSelectSource.UseVisualStyleBackColor = true;
+            this.btnSelectSource.Click += new System.EventHandler(this.btnSelectSource_Click);
+            // 
+            // btnDestination
+            // 
+            this.btnDestination.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDestination.Location = new System.Drawing.Point(469, 44);
+            this.btnDestination.Name = "btnDestination";
+            this.btnDestination.Size = new System.Drawing.Size(111, 23);
+            this.btnDestination.TabIndex = 7;
+            this.btnDestination.Text = "Select Destination";
+            this.btnDestination.UseVisualStyleBackColor = true;
+            this.btnDestination.Click += new System.EventHandler(this.btnDestination_Click);
+            // 
+            // btnClearLog
+            // 
+            this.btnClearLog.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClearLog.Location = new System.Drawing.Point(505, 238);
+            this.btnClearLog.Name = "btnClearLog";
+            this.btnClearLog.Size = new System.Drawing.Size(75, 23);
+            this.btnClearLog.TabIndex = 9;
+            this.btnClearLog.Text = "Clear Log";
+            this.btnClearLog.UseVisualStyleBackColor = true;
+            this.btnClearLog.Click += new System.EventHandler(this.btnClearLog_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLog.Location = new System.Drawing.Point(12, 80);
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(568, 118);
+            this.txtLog.TabIndex = 10;
+            this.txtLog.Text = "";
+            // 
+            // lblAbout
+            // 
+            this.lblAbout.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblAbout.Location = new System.Drawing.Point(12, 243);
+            this.lblAbout.Name = "lblAbout";
+            this.lblAbout.Size = new System.Drawing.Size(186, 23);
+            this.lblAbout.TabIndex = 11;
+            this.lblAbout.Text = "Author Notice";
+            // 
+            // chkTestMode
+            // 
+            this.chkTestMode.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.chkTestMode.AutoSize = true;
+            this.chkTestMode.Location = new System.Drawing.Point(204, 242);
+            this.chkTestMode.Name = "chkTestMode";
+            this.chkTestMode.Size = new System.Drawing.Size(77, 17);
+            this.chkTestMode.TabIndex = 12;
+            this.chkTestMode.Text = "Test Mode";
+            this.chkTestMode.UseVisualStyleBackColor = true;
+            this.chkTestMode.CheckedChanged += new System.EventHandler(this.chkTestMode_CheckedChanged);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.Location = new System.Drawing.Point(424, 238);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 13;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // pbFiles
+            // 
+            this.pbFiles.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbFiles.Location = new System.Drawing.Point(12, 204);
+            this.pbFiles.Name = "pbFiles";
+            this.pbFiles.Size = new System.Drawing.Size(568, 28);
+            this.pbFiles.Step = 1;
+            this.pbFiles.TabIndex = 14;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 273);
+            this.Controls.Add(this.pbFiles);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.chkTestMode);
+            this.Controls.Add(this.lblAbout);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnClearLog);
+            this.Controls.Add(this.btnDestination);
+            this.Controls.Add(this.btnSelectSource);
+            this.Controls.Add(this.txtDestinationDir);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSourceDir);
+            this.Controls.Add(this.btnSync);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(600, 300);
+            this.Name = "frmMain";
+            this.Text = "Jolly Roger\'s Directory Sync";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSync;
+        private System.Windows.Forms.TextBox txtSourceDir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDestinationDir;
+        private System.Windows.Forms.Button btnSelectSource;
+        private System.Windows.Forms.Button btnDestination;
+        private System.Windows.Forms.Button btnClearLog;
+        private System.Windows.Forms.RichTextBox txtLog;
+        private System.Windows.Forms.Label lblAbout;
+        private System.Windows.Forms.CheckBox chkTestMode;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.ProgressBar pbFiles;
+    }
+}
+
---
+++ DirectorySync/Main.cs
@@ -0,0 +1,401 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DirectoryCopy
+{
+    public partial class frmMain : Form
+    {
+        protected delegate void SetTextCallback(string text, bool flush_log);
+        protected delegate void UpdateProgressBar(int amount);
+
+        #region Fields
+
+            private static readonly int MAX_PATH = 260;
+
+            protected StringBuilder _MessageLog;
+            protected bool _TestMode;
+            protected volatile bool _HaltProcessing;
+            protected Form _MainForm;
+            protected Stopwatch _Timer;
+
+        #endregion
+
+        #region Methods
+
+            protected bool ValidatePaths()
+            {
+                if (!Directory.Exists(this.txtSourceDir.Text))
+                {
+                    LogMesage("Error: Source directory does not exist.");
+                    return false;
+                }
+
+                if (!Directory.Exists(this.txtDestinationDir.Text))
+                {
+                    LogMesage("Error: Destination directory does not exist.");
+                    return false;
+                }
+
+                if (this.txtSourceDir.Text == this.txtDestinationDir.Text)
+                {
+                    LogMesage("Error: Source directory and destination directory must be different.");
+                    return false;
+                }
+
+                return true;
+            } 
+
+            protected void RemoveFileAttributes(string directory, bool recursive)
+            {
+                DirectoryInfo directory_info = new DirectoryInfo(directory);
+
+                if (directory_info.Attributes != FileAttributes.Normal)
+                    directory_info.Attributes = FileAttributes.Normal;
+
+                string[] files = Directory.GetFiles(directory);
+
+                foreach (string file in files)
+                    File.SetAttributes(file, FileAttributes.Normal);
+
+                if (recursive)
+                {
+                    string[] directories = Directory.GetDirectories(directory);
+
+                    foreach (string subdirectory in directories)
+                        RemoveFileAttributes(subdirectory, recursive);
+                }
+            }
+
+            protected int CountFiles(string directory)
+            {
+                int count = Directory.GetFiles(directory).Length;
+
+                foreach (var sub_directory in Directory.GetDirectories(directory))
+                    count += CountFiles(sub_directory);
+                
+                return count;
+            }
+
+            protected async void SyncDirectory(string source_directory, string destination_directory)
+            {       
+                if (ValidatePaths())
+                {
+                    _Timer.Restart();
+
+                    LogMesage("Beginning Synchronization.", true);
+
+                    await Task.Run(() =>
+                    {
+                        SyncDirectory(source_directory, destination_directory, source_directory, destination_directory);
+                    });
+
+                    if (_HaltProcessing)
+                        LogMesage("Synchronization halted.", true);
+                    else
+                        LogMesage("Synchronization complete.", true);
+
+                    _Timer.Stop();
+                }
+            }
+
+            protected void SyncDirectory(string source_root, string destination_root, string source_directory, string destination_directory)
+            {
+                if (_HaltProcessing == true)
+                    return;       
+
+                int file_count = 0; 
+
+                LogMesage(string.Format("Starting sync of directory {0}.", source_directory));
+
+                if (!Directory.Exists(destination_directory))
+                {
+                    LogMesage("Creating " + destination_directory);
+
+                    if (!_TestMode)
+                        Directory.CreateDirectory(destination_directory);
+                }
+             
+                // remove non-existing files. Look at each file in the destination directory and if a 
+                // matching file does not exist in the source directory remove it.
+                if (Directory.Exists(destination_directory))
+                {
+                    foreach (string file in Directory.GetFiles(destination_directory))
+                    {
+                        string source_filename = source_directory + "\\" + Path.GetFileName(file);
+
+                        if (!File.Exists(source_filename))
+                        {
+                            if (!_TestMode)
+                            {
+                                var file_attributes = File.GetAttributes(file);
+
+                                if (file_attributes != FileAttributes.Normal)
+                                    File.SetAttributes(file, FileAttributes.Normal);
+
+                                File.Delete(file);
+                            }
+
+                            LogMesage("Deleting " + file);
+                        }
+                    }
+                }
+
+                // copy files.  
+                // If the file exists and doesn't match the last write time, overwrite                
+                if (Directory.Exists(source_directory))
+                {
+                    foreach (string item in Directory.GetFiles(source_directory))
+                    {
+                        string destination_filename = destination_directory + "\\"+ Path.GetFileName(item);
+
+                        if (File.Exists(destination_filename))
+                        {
+                            if (File.GetLastWriteTime(item) != File.GetLastWriteTime(destination_filename))
+                            {
+                                if (destination_filename.Length > 260)
+                                    LogMesage(string.Format("Path {0} is {1} characters long. This exceeds the {2} character limit.", destination_filename, destination_filename.Length, MAX_PATH));
+
+                                if (!_TestMode)
+                                {
+                                    // Make sure we are clear for move.
+
+                                    var file_attributes = File.GetAttributes(destination_filename);
+
+                                    if (file_attributes != FileAttributes.Normal)
+                                        File.SetAttributes(destination_filename, FileAttributes.Normal);
+
+                                    File.Copy(item, destination_filename, true);
+                                }
+
+                                LogMesage("Updating out of date file " + destination_filename);
+                            }
+                        }
+                        else
+                        {
+                            if (!_TestMode)
+                                File.Copy(item, destination_filename);
+
+                            LogMesage("Copying " + destination_filename);
+                        }
+
+                        file_count++;
+                    }
+                }
+
+                // remove any directories that exist in the destination but not in the source
+                // need to check for existance beacuse in test mode, new directories will not be created.
+                if (Directory.Exists(destination_directory))
+                {
+                    foreach (string directory in Directory.GetDirectories(destination_directory))    
+                    {
+                        string source = directory.Replace(destination_root, source_root);
+
+                        if (!Directory.Exists(source))
+                        {
+                            LogMesage("Deleting " + directory);
+
+                            if (!_TestMode)
+                            {
+                                DirectoryInfo di = new DirectoryInfo(directory);
+                                RemoveFileAttributes(directory, true);
+                                di.Delete(true);
+                            }
+                        }
+                    } 
+                }
+
+                string[] directory_list = Directory.GetDirectories(source_directory);
+
+                //// call function recursively on subdirectories
+                //foreach (var item in directory_list)    
+                //{
+                //    string destination = destination_root + item.Replace(source_root, string.Empty);
+                //    SyncDirectory(source_root, destination_root ,item, destination);
+                //}
+
+                Parallel.ForEach(directory_list, new ParallelOptions { MaxDegreeOfParallelism = 4 }, current_directory =>
+                {
+                    string destination = destination_root + current_directory.Replace(source_root, string.Empty);
+                    SyncDirectory(source_root, destination_root ,current_directory, destination);
+                });
+
+                LogMesage(string.Format("Sync of directory {0} complete.", source_directory), true);
+                UpdateProgress(file_count);
+            }
+
+            protected void UpdateProgress(int amount)
+            {
+                if (this.pbFiles.InvokeRequired)
+                {
+				    UpdateProgressBar delegate_call = new UpdateProgressBar(UpdateProgress);
+				    this.Invoke(delegate_call, new object[] { amount });
+                }
+                else
+                {
+                    this.pbFiles.Step = amount;
+                    this.pbFiles.PerformStep();
+
+                    Console.WriteLine("Amount: " + amount.ToString());
+                }
+            }
+
+            protected void LogMesage(string message)
+            {
+                LogMesage(message, false);
+            }
+
+            protected void LogMesage(string message, bool flush_log)
+            {
+                if (_MessageLog == null)
+                    _MessageLog = new StringBuilder();
+             
+                // only update UI every several seconds
+
+                if (this.txtLog.InvokeRequired)
+                {
+				    SetTextCallback delegate_call = new SetTextCallback(LogMesage);
+				    this.Invoke(delegate_call, new object[] { message, flush_log });
+                }
+                else
+                {
+                    _MessageLog.AppendLine(string.Format("<{0}> {1}", DateTime.Now.ToLongTimeString(), message));
+
+                    if (!_Timer.IsRunning)
+                    {
+                        this.txtLog.Text = _MessageLog.ToString();
+                    }
+                    else if (_Timer.ElapsedMilliseconds > 5000 || flush_log)
+                    { 
+                        this.txtLog.Text = _MessageLog.ToString();
+
+                        _Timer.Restart();
+                        _MessageLog.Clear();
+                    }
+                }
+            }
+
+            protected void SetTestMode()
+            {
+                _TestMode = this.chkTestMode.Checked;
+
+                if (_TestMode)
+                    LogMesage("Test mode is ON: No updates will be made.");
+                else
+                    LogMesage("Test mode is OFF: Updates will be made normally.");
+            }
+
+        #endregion
+
+        #region Events
+
+            public frmMain()
+            {
+                InitializeComponent();
+
+                this.Text               = "Jolly Roger's Directory Sync";
+                this.btnSync.Enabled    = false;
+                this.btnCancel.Enabled  = false;
+                this.lblAbout.Text      = "Written by Roger Hill, 2011";
+                
+                //_MessageWindow          = this.txtLog;
+                _MainForm               = this;
+                _Timer                  = new Stopwatch();
+
+                LogMesage("Jolly Roger's Directory Sync, Version " + Application.ProductVersion);
+                LogMesage("Please select a source and destination directory. Any content in the destination directory will be updated to match that of the source directory.");
+
+                SetTestMode();
+            }
+
+            private async void btnSync_Click(object sender, EventArgs e)
+            {
+                _HaltProcessing = false;
+
+                try
+                {
+                    this.pbFiles.Maximum = CountFiles(this.txtSourceDir.Text);
+                    this.pbFiles.Value = 0;
+
+                    await Task.Run(() =>
+                    {
+                        SyncDirectory(this.txtSourceDir.Text, this.txtDestinationDir.Text);
+                    });
+                }
+                catch (Exception ex)
+                {
+                    LogMesage("Error processing : " + ex.Message);
+                    LogMesage("Halting synchronization.");
+                }
+            }
+
+            private void btnSelectSource_Click(object sender, EventArgs e)
+            {
+                FolderBrowserDialog folder_picker       = new FolderBrowserDialog();
+                folder_picker.RootFolder                = Environment.SpecialFolder.MyComputer;
+                folder_picker.Description               = "Please select the source folder.";
+
+                if (this.txtSourceDir.Text != string.Empty)
+                    folder_picker.SelectedPath = this.txtSourceDir.Text;
+
+                DialogResult result = folder_picker.ShowDialog();
+
+                if (result == DialogResult.OK)
+                      this.txtSourceDir.Text = folder_picker.SelectedPath;
+            }
+
+            private void btnDestination_Click(object sender, EventArgs e)
+            {
+                FolderBrowserDialog folder_picker       = new FolderBrowserDialog();
+                folder_picker.RootFolder                = Environment.SpecialFolder.MyComputer;
+                folder_picker.Description               = "Please select the destination folder.";
+
+                DialogResult result = folder_picker.ShowDialog();
+
+                if (result == DialogResult.OK)
+                    this.txtDestinationDir.Text = folder_picker.SelectedPath;
+            }
+
+            private void btnClearLog_Click(object sender, EventArgs e)
+            {
+                _MessageLog = null;
+                LogMesage("Log Cleared.");
+                this.pbFiles.Value = 0;
+            }
+
+            private void btnCancel_Click(object sender, EventArgs e)
+            {
+                if (!_HaltProcessing)
+                { 
+                    _HaltProcessing = true;
+                    LogMesage("Attempting to halt synchronization...");
+                }
+            }
+
+            private void txtSourceDir_TextChanged(object sender, EventArgs e)
+            {
+                bool ready_to_sync      = (this.txtDestinationDir.Text.Length > 0) && (this.txtSourceDir.Text.Length > 0);
+
+                this.btnSync.Enabled    = ready_to_sync;
+                this.btnCancel.Enabled  = ready_to_sync;
+            }
+
+            private void txtDestinationDir_TextChanged(object sender, EventArgs e)
+            {
+                bool ready_to_sync      = (this.txtDestinationDir.Text.Length > 0) && (this.txtSourceDir.Text.Length > 0);
+
+                this.btnSync.Enabled    = ready_to_sync;
+                this.btnCancel.Enabled  = ready_to_sync;
+            }
+
+            private void chkTestMode_CheckedChanged(object sender, EventArgs e)
+            {
+                SetTestMode();
+            }        
+
+        #endregion
+    }
+}
